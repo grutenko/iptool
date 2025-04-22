934c250915d4854a7a62f89815e2abf96bf1b4aa
alexey <alexfedor932@gmail.com>
2025-04-22 04:16:38 +0300

    misc

--- proc.h
+++ proc.h
@@ -4,9 +4,13 @@
 #include "iap.h"
 #include <stdio.h>
 
-int cmd_invert_proc(int cmd_opt_c, struct cmd_opt *cmd_opts, struct cmd_in *in, FILE *out);
-int cmd_filter_proc(int cmd_opt_c, struct cmd_opt *cmd_opts, struct cmd_in *in, FILE *out);
-int cmd_deflate_proc(int cmd_opt_c, struct cmd_opt *cmd_opts, struct cmd_in *in, FILE *out);
-int cmd_inflate_proc(int cmd_opt_c, struct cmd_opt *cmd_opts, struct cmd_in *in, FILE *out);
+int cmd_invert_proc(int cmd_opt_c, struct cmd_opt *cmd_opts, struct cmd_in *in,
+                    FILE *out);
+int cmd_filter_proc(int cmd_opt_c, struct cmd_opt *cmd_opts, struct cmd_in *in,
+                    FILE *out);
+int cmd_deflate_proc(int cmd_opt_c, struct cmd_opt *cmd_opts, struct cmd_in *in,
+                     FILE *out);
+int cmd_inflate_proc(int cmd_opt_c, struct cmd_opt *cmd_opts, struct cmd_in *in,
+                     FILE *out);
 
 #endif

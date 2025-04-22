9aeea8e14e66aeb462e97bdfff5e22a62514e7d0
alexey <alexfedor932@gmail.com>
2025-04-22 23:29:03 +0300

    misc

--- ip.h
+++ ip.h
@@ -18,18 +18,123 @@ enum {
 
 typedef void(*iap_walk_proc_p)(const iap_t *a, int depth, int mode);
 
+/**
+ * @brief Return raw subnet mask
+ *
+ * @param cidr
+ * @return raw mask
+ */
 unsigned int iap_mask(int cidr);
+/**
+ * @brief return raw repr for address
+ *
+ * @param net address struct
+ * @return raw address
+ */
 unsigned int iap_raw(const iap_t *net);
+/**
+ * @brief Return first address in range
+ *
+ * @param net address struct
+ * @param from first address in range
+ * @return void
+ */
 void iap_from(const iap_t *net, iap_t *from);
+/**
+ * @brief Return last address in range
+ *
+ * @param net address struct
+ * @param to last address in range
+ * @return void
+ */
 void iap_to(const iap_t *net, iap_t *to);
+/**
+ * @brief Increment address
+ *
+ * @param net address struct
+ * @return void
+ */
 void iap_inc(iap_t *net);
+/**
+ * @brief Compare two addresses
+ *
+ * @param net0 first address
+ * @param net1 second address
+ * @return int
+ */
 int iap_in(const iap_t *net0, const iap_t *net1);
+/**
+ * @brief Compare two addresses
+ *
+ * @param net0 first address
+ * @param net1 second address
+ * @return int
+ */
+int iap_cmp(const iap_t *net0, const iap_t *net1);
+/**
+ * @brief Insert address into tree
+ *
+ * @param root root of tree
+ * @param new address to insert
+ * @return iap_t*
+ */
 iap_t *iap_insert(iap_t **root, const iap_t *new);
+/**
+ * @brief Remove addresses from tree by subnet
+ *
+ * @param root root of tree
+ * @param net address to prune
+ * @return void
+ */
 void iap_prune(iap_t **root, const iap_t *net);
+/**
+ * @brief Remove address from tree by concrete address and subnet
+ *
+ * @param root root of tree
+ * @param net address to remove
+ * @return void
+ */
 void iap_remove(iap_t **root, const iap_t *net);
+/**
+ * @brief Free tree
+ *
+ * @param root root of tree
+ * @return void
+ */
 void iap_free(iap_t **root);
+/**
+ * @brief Walk tree
+ *
+ * @param root root of tree
+ * @param proc callback function
+ * @return void
+ */
 void iap_walk(const iap_t *root, iap_walk_proc_p proc);
+/**
+ * @brief Convert address to string
+ *
+ * @param net address struct
+ * @param out output buffer
+ * @return return length of string
+ */
 int iap_ntoa(const iap_t *net, char *out);
+/**
+ * @brief Convert string to address
+ *
+ * @param str input string
+ * @param size size of input string
+ * @param out output buffer
+ * @return return length of string or 0 if address is invalid
+ */
 int iap_aton(const char *str, int size, iap_t *out);
+/**
+ * @brief Create tree with subnets with all addresses in range
+ *
+ * @param from first address
+ * @param to last address
+ * @param root root of tree
+ * @return return count of inserted addresses or 0 if memory allocation failed
+ */
+int iap_range(const iap_t *from, const iap_t *to, iap_t **root);
 
 #endif
--- proc.h
+++ proc.h
@@ -4,9 +4,33 @@
 #include "ip.h"
 #include <stdio.h>
 
+/**
+ * Invert the addresses in the tree.
+ * @param r root of tree
+ * @param out output stream
+ * @return 0 on success, -1 on error
+ */
 int cmd_invert_proc(iap_t **r, FILE *out);
+/**
+ * Filter the addresses in the tree.
+ * @param r root of tree
+ * @param out output stream
+ * @return 0 on success, -1 on error
+ */
 int cmd_filter_proc(iap_t **r, FILE *out);
+/**
+ * Deflate the addresses in the tree.
+ * @param r root of tree
+ * @param out output stream
+ * @return 0 on success, -1 on error
+ */
 int cmd_deflate_proc(iap_t **r, FILE *out);
+/**
+ * Inflate the addresses in the tree.
+ * @param r root of tree
+ * @param out output stream
+ * @return 0 on success, -1 on error
+ */
 int cmd_inflate_proc(iap_t **r, FILE *out);
 
 #endif
